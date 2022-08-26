1c4a41332e91e809216380a4856a19a5f370dea2
Emmanuel Seyon <noyysey@gmail.com>
2022-08-26 11:27:31 -0500

    Dashboard v2.1 Manny

---
+++ src/main/java/com/example/demo/DashBoardController2.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public interface DashBoardController2 extends Initializable {
+    void intialize (URL url, ResourceBundle rb);
+}
