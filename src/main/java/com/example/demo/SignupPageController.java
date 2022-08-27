8e74cecace9159457dd4c63da303189a686cde6e
Pandora Vang <pandora.vaj@gmail.com>
2022-08-26 20:34:20 -0500

    Service 5.0 SignupPageController.java - fixed SSN and Phone Number verify alerts - Create method that checks for age < 16

--- src/main/java/com/example/demo/SignupPageController.java
+++ src/main/java/com/example/demo/SignupPageController.java
@@ -10,36 +10,35 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.Period;
 
 
 @SuppressWarnings("ALL")
 public class SignupPageController {
     @FXML
     private TextField SocialSecurityF, UsernameF, PasswordF, PhoneNumberF, EmailF,
-            firstName, LastNameF, FirstNameF, Street, MiddleNameF, City, ZipCode, Apartment, gender, maritalStatus ;
-    private String  genderSet ="";
+            firstName, LastNameF, FirstNameF, Street, MiddleNameF, City, ZipCode, Apartment, gender, maritalStatus;
+    private String genderSet = "";
     private String maritalSet = "";
     @FXML
-    private Label DOBLabel,firstName1, LastName, MiddleName, SocialSecurity, Username, Password,
+    private Label DOBLabel, firstName1, LastName, MiddleName, SocialSecurity, Username, Password,
             PhoneNumber, Email, SignupLabel, verifySSN, verifyPhone;
     @FXML
     private RadioButton male;
-
     @FXML
     private RadioButton female;
     @FXML
     private RadioButton other;
     @FXML
-    private RadioButton single,married;
-
-
-    private Stage stage; private Scene scene; private Parent root;
+    private RadioButton single, married;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Button Home, SignupButton;
     @FXML
@@ -48,10 +47,12 @@ public class SignupPageController {
     private AnchorPane anchorPane;
     @FXML
     private int age;
+
     @FXML
     public void getDate(ActionEvent event) {
         LocalDate date = datePicker.getValue();
     }
+
     @FXML
     protected void Home(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("main.fxml"));
@@ -60,6 +61,7 @@ public class SignupPageController {
         stage.setScene(scene);
         stage.show();
     }
+
     @FXML
     protected void Signup(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
@@ -68,42 +70,46 @@ public class SignupPageController {
         stage.setScene(scene);
         stage.show();
     }
+
     public void verify(ActionEvent actionEvent) throws Exception {
-        String [] check = new String[]{FirstNameF.getText(),LastNameF.getText(),SocialSecurityF.getText(),
-                UsernameF.getText(), PasswordF.getText(), PhoneNumberF.getText(), EmailF.getText()};
+        String[] verifyTextfield = new String[]{FirstNameF.getText(), LastNameF.getText(), SocialSecurityF.getText(),
+                UsernameF.getText(), PasswordF.getText(), PhoneNumberF.getText(), EmailF.getText(), Street.getText(), City.getText(), ZipCode.getText(), Apartment.getText()};
+        TextField[] textFields = new TextField[]{FirstNameF, LastNameF, SocialSecurityF, UsernameF, PasswordF, PhoneNumberF, EmailF, Street, City, ZipCode, Apartment};
+        for (int i = 0; i < verifyTextfield.length; i++) {
+            if (verifyTextfield[i].isBlank()) {
+                textFields[i].setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+            } else if (!verifyTextfield[i].isBlank()) {
+                textFields[i].setStyle("-fx-text-box-border: #D3D3D3; -fx-focus-color: #D3D3D3;");
+            }
+        }
 
+        if (SocialSecurityF.getText().length() != 9 || PhoneNumberF.getText().length() != 10 || checkSSNInfo() == false || checkPhoneInfo() == false || ageVerify() == false) {
 
-        if(FirstNameF.getText().isBlank() ||
-                LastNameF.getText().isBlank()||
-                SocialSecurityF.getText().isBlank()||
-                UsernameF.getText().isBlank()||
-                PasswordF.getText().isBlank()||
-                PhoneNumberF.getText().isBlank() ||
-                EmailF.getText().isBlank()){
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Incompleted Fields");
-            alert.setHeaderText("You have incompleted fields");
-            alert.setContentText("You have incompleted fields");
-            alert.show();
-        } else if (SocialSecurityF.getText().length() != 9 || PhoneNumberF.getText().length() != 10 || checkSSNInfo() == false || checkPhoneInfo() == false) {
-            if(SocialSecurityF.getText().length() != 9){
-                verifySSN.setText("* Your Social Security Number is invalid.");
+            if (SocialSecurityF.getText().length() != 9 || checkSSNInfo() == false) {
+                SocialSecurityF.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+                verifySSN.setText("Your Social Security Number is invalid.");
+            } else {
+                verifySSN.setText("");
             }
-            if(checkSSNInfo() == false){
-                verifySSN.setText("Invalid Social Sercurity Format");
+
+             if (checkPhoneInfo() == false || PhoneNumberF.getText().length() != 10) {
+                PhoneNumberF.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+                verifyPhone.setText("Your phone number is invalid.");
             }
-            if(PhoneNumberF.getText().length() != 10){
-                verifyPhone.setText("* Your phone number is invalid.");
+            else {
+                verifyPhone.setText("");
             }
-            if(checkPhoneInfo() == false){
-                verifyPhone.setText("Invalid Phone Number Format");
+            if (ageVerify() == true){
+                datePicker.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+
             }
-        } else{
+
+        } else {
+            verifySSN.setText("");
+            verifyPhone.setText("");
             saveInfo(actionEvent);
         }
-
-
-        }
+    }
 
     //checks for age - ssn & phone = number - if user exists
     public boolean checkSSNInfo() throws Exception, ArithmeticException {
@@ -113,9 +119,9 @@ public class SignupPageController {
             return true;
         } catch (NumberFormatException e) {
             return false;
-
         }
     }
+
     public boolean checkPhoneInfo() throws Exception, ArithmeticException {
 
         try {
@@ -126,38 +132,63 @@ public class SignupPageController {
 
         }
     }
+    public boolean ageVerify() {
+        LocalDate today = LocalDate.now();
+        LocalDate birthday = LocalDate.of(datePicker.getValue().getYear(),
+                datePicker.getValue().getMonth(),
+                datePicker.getValue().getDayOfMonth());
+        LocalDate verifyAge = today.minusYears(birthday.getYear());
+        System.out.println(verifyAge.getYear());
 
-    public String genderCheck(){
+        if (verifyAge.getYear() < 16) {
+            System.out.println(1);
+            return false;
+        } else if (verifyAge.getYear() == 16 && today.getMonthValue() < birthday.getMonthValue()) {
+            System.out.println(2);
+            return false;
+        } else if (verifyAge.getYear() == 16 && today.getMonthValue() == birthday.getMonthValue() && today.getDayOfMonth() < birthday.getDayOfMonth()) {
+            System.out.println(3);
+            return false;
+        } else {
+            System.out.println("you are old");
+            return true;
+        }
+    }
 
-        if(male.isSelected() ){
+
+
+    public String genderCheck() {
+
+        if (male.isSelected()) {
             genderSet = "M";
         } else if (female.isSelected()) {
             genderSet = "F";
 
-        }
-        else if(other.isSelected()){
+        } else if (other.isSelected()) {
             genderSet = "O";
         }
         return genderSet;
     }
 
-    public String maritalCheck(){
+    public String maritalCheck() {
 
-        if(single.isSelected() ){
+        if (single.isSelected()) {
             maritalSet = "single";
         } else if (married.isSelected()) {
             maritalSet = "married";
         }
         return maritalSet;
     }
-  public void checkEmptyFields(){
 
-  }
+    public void checkEmptyFields() {
+
+    }
+
 
     public void saveInfo(ActionEvent actionEvent) throws SQLException, IOException {
         Connectivity connectivity = new Connectivity();
         Connection connection = connectivity.getConnection();
-          Object object = new Object();
+        Object object = new Object();
         Customer customer = new Customer(FirstNameF.getText(),
                 LastNameF.getText(),
                 MiddleNameF.getText(),
@@ -173,31 +204,31 @@ public class SignupPageController {
                 City.getText(),
                 "mn",
                 ZipCode.getText(),
-                Apartment.getText(),genderCheck(), maritalCheck()
-                );
+                Apartment.getText(), genderCheck(), maritalCheck()
+        );
         System.out.println(customer);
 
         String query = "insert into customer_personal_info(" + "First_name,Last_name,middle_name,date_of_birth,address,zipp_code,Country,states,city,contact_no, ssn,username,password,email,gender,marital_status) values "
-                 + "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                + "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         PreparedStatement statement = connection.prepareStatement(query);
         statement.setString(1, customer.getFirstName());
         statement.setString(2, customer.getLastName());
-        statement.setString(3,customer.getMiddleName()) ;
-        statement.setString(4,customer.getDateOfBirth().toString());
-        statement.setString(5,customer.getStreet());
-        statement.setString(6,customer.getZipCode());
-        statement.setString(7,"US");
+        statement.setString(3, customer.getMiddleName());
+        statement.setString(4, customer.getDateOfBirth().toString());
+        statement.setString(5, customer.getStreet());
+        statement.setString(6, customer.getZipCode());
+        statement.setString(7, "US");
         statement.setString(8, "MN");
-        statement.setString(9,customer.getCity());
-        statement.setString(10,customer.getPhoneNumber());
+        statement.setString(9, customer.getCity());
+        statement.setString(10, customer.getPhoneNumber());
         statement.setString(11, customer.getSSN());
-        statement.setString(12,customer.getUsername());
+        statement.setString(12, customer.getUsername());
         statement.setString(13, customer.getPassword());
-        statement.setString(14,customer.getEmail());
-        statement.setString(15,customer.getGender());
-        statement.setString(16,customer.getMartialStatus());
+        statement.setString(14, customer.getEmail());
+        statement.setString(15, customer.getGender());
+        statement.setString(16, customer.getMartialStatus());
         statement.executeUpdate();
         System.out.println(customer.getEmail());
         connection.close();
-   }
+    }
 }
