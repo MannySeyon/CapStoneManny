34246100950a912fc0896e52ec0c53529a47a65f
Pandora Vang <pandora.vaj@gmail.com>
2022-08-29 10:45:01 -0500

    Revert "service 4.0"
    This reverts commit bb77a39f5ae1ef6162cf73569519a0ddc6989821.

--- src/main/java/com/example/demo/SignupPageController.java
+++ src/main/java/com/example/demo/SignupPageController.java
@@ -1,35 +1,26 @@
 package com.example.demo;
 
 import com.example.demo.customer.Customer;
-import javafx.beans.Observable;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 @SuppressWarnings("ALL")
-
-public class SignupPageController implements Initializable {
+public class SignupPageController {
     @FXML
     private TextField SocialSecurityF, UsernameF, PasswordF, PhoneNumberF, EmailF,
             firstName, LastNameF, FirstNameF, Street, MiddleNameF, City, ZipCode, Apartment, gender, maritalStatus;
@@ -37,7 +28,7 @@ public class SignupPageController implements Initializable {
     private String maritalSet = "";
     @FXML
     private Label DOBLabel, firstName1, LastName, MiddleName, SocialSecurity, Username, Password,
-            PhoneNumber, Email, SignupLabel, verifySSN, verifyPhone, verifyAgeLabel, usernameWarning;
+            PhoneNumber, Email, SignupLabel, verifySSN, verifyPhone, verifyAgeLabel, progressLabel, progressPercentLabel;
     @FXML
     private RadioButton male;
     @FXML
@@ -57,14 +48,11 @@ public class SignupPageController implements Initializable {
     private AnchorPane anchorPane;
     @FXML
     private int age;
-
-
     @FXML
-    private ComboBox<String> comboBox;
-
-
-    ObservableList<String> list = FXCollections.observableArrayList("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon ", "Pennsylvania ", "Rhode Island", "South Carolina ", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming");
-
+    private Button progressButton;
+    @FXML
+    private ProgressBar progressBar;
+    BigDecimal progress = new BigDecimal(String.format("%.2f", 0.0));
 
     @FXML
     public void getDate(ActionEvent event) {
@@ -82,14 +70,12 @@ public class SignupPageController implements Initializable {
 
     @FXML
     protected void Signup(ActionEvent event) throws IOException {
-
         root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
 
     public void verify(ActionEvent actionEvent) throws Exception {
         String[] verifyTextfield = new String[]{FirstNameF.getText(), LastNameF.getText(), SocialSecurityF.getText(),
@@ -102,22 +88,17 @@ public class SignupPageController implements Initializable {
                 textFields[i].setStyle("-fx-text-box-border: #D3D3D3; -fx-focus-color: #D3D3D3;");
             }
         }
-        if (datePicker.getValue() == null) {
-            datePicker.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
-        } else{
-             datePicker.setStyle("-fx-text-box-border: #D3D3D3; -fx-focus-color: #D3D3D3;");
-    }
 
-        if (checkSSNInfo() == false || checkPhoneInfo() == false || ageVerify() == false || passwordVerify() == false || datePicker.getValue() == null ) {
+        if (SocialSecurityF.getText().length() != 9 || PhoneNumberF.getText().length() != 10 || checkSSNInfo() == false || checkPhoneInfo() == false || ageVerify() == false) {
 
-            if (checkSSNInfo() == false) {
+            if (SocialSecurityF.getText().length() != 9 || checkSSNInfo() == false) {
                 SocialSecurityF.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
                 verifySSN.setText("Your Social Security Number is invalid.");
             } else {
                 verifySSN.setText("");
             }
 
-            if (checkPhoneInfo() == false ) {
+            if (checkPhoneInfo() == false || PhoneNumberF.getText().length() != 10) {
                 PhoneNumberF.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
                 verifyPhone.setText("Your phone number is invalid.");
             } else {
@@ -129,28 +110,19 @@ public class SignupPageController implements Initializable {
             } else {
                 verifyAgeLabel.setText("");
             }
-            if (passwordVerify() == false) {
-            }
-            if (UsernameF.getText().length() < 5 || UsernameF.getText().length() > 20) {
-                usernameWarning.setText("user name must be between  6 and 19 charachters ");
-            }
 
         } else {
             verifySSN.setText("");
             verifyPhone.setText("");
             verifyAgeLabel.setText("");
-            usernameWarning.setText("");
             saveInfo(actionEvent);
         }
     }
-
 
     //checks for age - ssn & phone = number - if user exists
     public boolean checkSSNInfo() throws Exception, ArithmeticException {
 
         try {
-            if(SocialSecurityF.getText().length() != 9)
-                return false;
             int intValue = Integer.parseInt(SocialSecurityF.getText());
             return true;
         } catch (NumberFormatException e) {
@@ -161,17 +133,12 @@ public class SignupPageController implements Initializable {
     public boolean checkPhoneInfo() throws Exception, ArithmeticException {
 
         try {
-            if (PhoneNumberF.getText().length() != 10)
-                return false;
             int intValue = Integer.parseInt(PhoneNumberF.getText());
             return true;
-
         } catch (NumberFormatException e) {
             return false;
 
         }
-
-
     }
 
     public boolean ageVerify() {
@@ -191,39 +158,10 @@ public class SignupPageController implements Initializable {
         }
     }
 
-    public boolean passwordVerify() {
-
-        Pattern pattern = Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{6,15}");
-        Matcher matcher = pattern.matcher(PasswordF.getText());
-        if (matcher.find() && matcher.group().equals(PasswordF.getText())) {
-            return true;
-        } else
-            return false;
-
-    }
-
-    public boolean emailVerify() {
-
-        Pattern pattern = Pattern.compile("[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]");
-        Matcher matcher = pattern.matcher(EmailF.getText());
-        if (matcher.find() && matcher.group().equals(EmailF.getText())) {
-            return true;
-        } else
-            return false;
-
-    }
-
-//    public boolean userExistsVerify() {
-//        Connectivity connectivity = new Connectivity();
-//        Connection connection = connectivity.getConnection();
-//        String query = "select count(1) where ssn=?  or (First_name=? and Last_name=? and date_of_birth=?) ";
-//
-//    }
-
 
     public String genderCheck() {
 
-        if (male.isSelected()){
+        if (male.isSelected()) {
             genderSet = "M";
         } else if (female.isSelected()) {
             genderSet = "F";
@@ -266,7 +204,7 @@ public class SignupPageController implements Initializable {
                 PasswordF.getText(),
                 Street.getText(),
                 City.getText(),
-                comboBox.getValue(),
+                "mn",
                 ZipCode.getText(),
                 Apartment.getText(), genderCheck(), maritalCheck()
         );
@@ -282,7 +220,7 @@ public class SignupPageController implements Initializable {
         statement.setString(5, customer.getStreet());
         statement.setString(6, customer.getZipCode());
         statement.setString(7, "US");
-        statement.setString(8, comboBox.getValue());
+        statement.setString(8, "MN");
         statement.setString(9, customer.getCity());
         statement.setString(10, customer.getPhoneNumber());
         statement.setString(11, customer.getSSN());
@@ -292,12 +230,50 @@ public class SignupPageController implements Initializable {
         statement.setString(15, customer.getGender());
         statement.setString(16, customer.getMartialStatus());
         statement.executeUpdate();
-        System.out.println(comboBox.getValue());
+        System.out.println(customer.getEmail());
         connection.close();
     }
+    @FXML  //Set progress percents =to Scene pages for Sign up
+    protected void NextPage(ActionEvent event) throws IOException {
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        comboBox.setItems(list);
+        root = FXMLLoader.load(getClass().getResource("SignUpPage2.fxml"));
+        //get Source cast to a node //Pass node to Stage
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //Pass the new fxml path to scene variable
+        scene = new Scene(root);
+        //pass scene to stage
+        stage.setScene(scene);
+        stage.show();
+        //Method to access value in progress
+        if (progress.doubleValue() < 1) {
+            //must keep reInstantiate BigDecimals
+            progress = new BigDecimal(String.format("%.2f", progress.doubleValue() + 0.25));
+            progressBar.setProgress(progress.doubleValue());
+            //Set text to progress *100 add % string to end //change double to Integer and round
+            //progressLabel.setText(Integer.toString((int) Math.round(progress.doubleValue() * 100)) + "%");
+            progressPercentLabel.setText(Integer.toString((int) (progress.doubleValue() * 100)) + "%");
+        }
     }
-}
+    @FXML
+    protected void PreviousPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
+        //get Source cast to a node //Pass node to Stage
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //Pass the new fxml path to scene variable
+        scene = new Scene(root);
+        //pass scene to stage
+        stage.setScene(scene);
+        stage.show();
+        //Method to access value in progress
+        if (progress.doubleValue() < 1) {
+            //must keep reInstantiate BigDecimals
+            progress = new BigDecimal(String.format("%.2f", progress.doubleValue() + -0.25));
+            progressBar.setProgress(progress.doubleValue());
+            //Set text to progress *100 add % string to end //change double to Integer and round
+            //progressLabel.setText(Integer.toString((int) Math.round(progress.doubleValue() * 100)) + "%");
+            progressPercentLabel.setText(Integer.toString((int) (progress.doubleValue() * 100)) + "%");
+        }
+
+    }
+    }
+
