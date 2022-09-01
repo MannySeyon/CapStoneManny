55687efc4f74873642f3d46f2538c8a3fe39704d
DHall0 <110750425+DHall0@users.noreply.github.com>
2022-09-01 11:53:27 -0500

    dav31

--- src/main/java/com/example/demo/Admin/CreateClientController.java
+++ src/main/java/com/example/demo/Admin/CreateClientController.java
@@ -1,11 +1,16 @@
 package com.example.demo.Admin;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class CreateClientController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class CreateClientController implements Initializable {
     public TextField first_name_fld;
     public TextField last_name_fld;
     public TextField password_fld;
@@ -17,4 +22,9 @@ public class CreateClientController {
     public TextField sv_amount_fld;
     public Button create_client_btn;
     public Label error_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
