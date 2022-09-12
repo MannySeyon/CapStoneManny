248796d44ffc1a3d0abda877f4de1d2e5f3d0edb
DHall0 <110750425+DHall0@users.noreply.github.com>
2022-09-12 07:36:02 -0500

    davidalmostthere

--- src/main/java/com/example/demo/Admin/AdminDashController.java
+++ src/main/java/com/example/demo/Admin/AdminDashController.java
@@ -1,6 +1,8 @@
 package com.example.demo.Admin;
 
 
+import com.example.demo.Connectivity;
+import com.example.demo.customer.Customer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,84 +13,155 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class AdminDashController implements Initializable {
-    //First Pane create clients
+    //First splitPane create clients
     @FXML
-    public TextField FName_fld;
+    private TextField FName_fld;
     @FXML
-    public TextField MName_fld;
+    private TextField MName_fld;
     @FXML
-    public TextField Ssn_fld;
+    private TextField Ssn_fld;
     @FXML
-    public TextField LName_fld;
+    private TextField LName_fld;
     @FXML
-    public TextField Phone_no_fld;
+    private TextField Phone_no_fld;
     @FXML
-    public DatePicker DOB_fld;
+    private DatePicker DOB_fld;
     @FXML
-    public RadioButton Single_radio_btn;
+    private RadioButton Single_radio_btn;
     @FXML
-    public RadioButton Married_radio_btn;
+    private RadioButton Married_radio_btn;
     @FXML
-    public RadioButton Male_radio_btn;
+    private RadioButton Male_radio_btn;
     @FXML
-    public RadioButton Female_radio_btn;
+    private RadioButton Female_radio_btn;
     @FXML
-    public RadioButton Non_binary_radio_btn;
-    //Second Pane clients listview
+    private RadioButton Non_binary_radio_btn;
+    //Second splitPane clients listview
     @FXML
-    public TextField email_fld;
+    private TextField email_fld;
     @FXML
-    public TextField Address_fld;
+    private TextField Address_fld;
     @FXML
-    public TextField city_fld;
+    private TextField city_fld;
     @FXML
-    public TextField zipcode_fld;
+    private TextField zipcode_fld;
     @FXML
-    public TextField username_fld;
+    private TextField username_fld;
     @FXML
-    public TextField password_fld;
+    private TextField password_fld;
     @FXML
-    public CheckBox ck_acc_cb;
+    private CheckBox ck_acc_cb;
     @FXML
-    public CheckBox sv_acc_cb;
+    private CheckBox sv_acc_cb;
     @FXML
-    public TextField ck_acc_bal_fld;
+    private TextField ck_acc_bal_fld;
     @FXML
-    public TextField sv_acc_bal_fld;
+    private TextField sv_acc_bal_fld;
     @FXML
-    public Button Create_new_btn;
+    private Button Create_new_btn;
     @FXML
-    public ChoiceBox state_fld;
+    private ChoiceBox state_fld;
 
     //Third Pane clients deposit
     @FXML
-    public TextField rusername_fld;
+    private TextField rusername_fld;
     @FXML
-    public Button search_btn;
+    private Button search_btn;
     @FXML
-    public ListView result_listview;
+    private TextField ck_amount_fld;
     @FXML
-    public TextField ck_amount_fld;
+    private Button ck_deposit_btn;
     @FXML
-    public Button ck_deposit_btn;
+    private TextField sv_amount_fld;
     @FXML
-    public TextField sv_amount_fld;
+    private Button sv_deposit_btn;
     @FXML
-    public Button sv_deposit_btn;
+    private TextField search_database_fld;
     @FXML
-    public TextField search_database_fld;
+    private Button Search_Client_btn;
     @FXML
-    public Button Search_Client_btn;
+    private TextField raddress_fld;
     @FXML
-    public TextField raddress_fld;
+    private Button Delete_btn;
+    //Transaction table
+    @FXML
+    private TableView Transaction_table;
+    @FXML
+    private TableColumn col_Transaction_Number;
+    @FXML
+    private TableColumn col_Account_Number;
+    @FXML
+    private TableColumn col_Date_created;
+    @FXML
+    private TableColumn col_income_date;
+    @FXML
+    private TableColumn col_income_deposit;
+    @FXML
+    private TableColumn col_income_amount;
+    @FXML
+    private TableColumn col_expense_date;
+    @FXML
+    private TableColumn col_expense_supplier;
+    @FXML
+    private TableColumn col_expense_amount;
+    //Checking table
+    @FXML
+    private TableView Checking_table;
+    @FXML
+    private TableColumn col_checking_acc_number;
+    @FXML
+    private TableColumn col_account_number;
+    @FXML
+    private TableColumn col_account_balance;
+    @FXML
+    private TextField ck_amount_deposit_fld;
+    @FXML
+    private Button ck_amount_deposit_btn;
+    //Savings table
+    @FXML
+    private TableView Savings_table;
+    @FXML
+    private TableColumn col_saving_acc_number;
+    @FXML
+    private TableColumn col_acc_number;
+    @FXML
+    private TableColumn col_acc_bal;
+    @FXML
+    private TextField sv_amount_deposit_fld;
+    @FXML
+    private Button sv_amount_deposit_btn;
+    @FXML
+    private TextField Apartment_fld;
+    private TextField search_db_fld;
+
+
+    // sets option choices
+    @FXML
+    private String maritalSet = "";
+    @FXML
+    private String genderSet = "";
+
+    @FXML
+    private Label verifyPhoneLabel;
+    @FXML
+    private Label verifySSNLabel;
+    @FXML
+    private Label verifyAgeLabel;
+    @FXML
+    private Label genderLabel;
+
+    @FXML
+    private Button Update_btn;
+    @FXML
+    private ComboBox<String> state_ComboBox;
     @FXML
     private SplitPane pnCreateClient;
     @FXML
@@ -132,6 +205,57 @@ public class AdminDashController implements Initializable {
     private TableColumn<ConnTable, String> col_email;
 
     ObservableList<ConnTable>clientsList = FXCollections.observableArrayList();
+    ObservableList<String> stateList = FXCollections.observableArrayList(
+            "Alabama",
+            "Alaska",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "Florida",
+            "Georgia",
+            "Hawaii",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Iowa",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine",
+            "Maryland",
+            "Massachusetts",
+            "Michigan",
+            "Minnesota",
+            "Mississippi",
+            "Missouri",
+            "Montana",
+            "Nebraska",
+            "Nevada",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "New York",
+            "North Carolina",
+            "North Dakota",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington",
+            "West Virginia",
+            "Wisconsin",
+            "Wyoming");
 
 
 
@@ -151,14 +275,198 @@ public class AdminDashController implements Initializable {
     @FXML
     private void createClientActions(ActionEvent event) {
 
+
+
     }
     @FXML
     protected void ClientListActions(ActionEvent actionEvent) {
     }
     @FXML
     protected void depositActions(ActionEvent actionEvent) {
+
+    }
+  public boolean CheckDate(){
+        if (DOB_fld.getValue()==null){
+            return false;
+        }
+        return true;
     }
 
+    public void verify(ActionEvent actionEvent) throws Exception {
+
+        String[] verifyTextfield = new String[]{FName_fld.getText(), LName_fld.getText(), Ssn_fld.getText(),
+                username_fld.getText(), password_fld.getText(), Phone_no_fld.getText(), email_fld.getText(), Address_fld.getText(), city_fld.getText(), zipcode_fld.getText(), MName_fld.getText()};
+
+        TextField[] textFields = new TextField[]{FName_fld, LName_fld, Ssn_fld, username_fld, password_fld, Phone_no_fld, email_fld, Address_fld, city_fld, zipcode_fld, MName_fld};
+
+        if(CheckDate()==false) {
+            DOB_fld.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+        }
+
+
+        for (int i = 0; i < verifyTextfield.length; i++) {
+            if (verifyTextfield[i].isBlank()) {
+                textFields[i].setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+            } else if (!verifyTextfield[i].isBlank()) {
+                textFields[i].setStyle("-fx-text-box-border: #D3D3D3; -fx-focus-color: #D3D3D3;");
+            }
+        }
+        if (Ssn_fld.getText().length() != 9 || Phone_no_fld.getText().length() != 10 || checkSSNInfo() == false || checkPhoneInfo() == false || ageVerify() == false) {
+
+            if (Ssn_fld.getText().length() != 9 || checkSSNInfo() == false) {
+                Ssn_fld.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+                verifySSNLabel.setText("Your Social Security Number is invalid.");
+            } else {
+                verifySSNLabel.setText("");
+            }
+
+            if (checkPhoneInfo() == false || Phone_no_fld.getText().length() != 10) {
+                Phone_no_fld
+                        .setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+                verifyPhoneLabel.setText("Your phone number is invalid.");
+            } else {
+                verifyPhoneLabel.setText("");
+            }
+            if (ageVerify() == false) {
+                DOB_fld.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+                verifyAgeLabel.setText("You are not eligable to create an account");
+            } else {
+                verifyAgeLabel.setText("");
+            }
+            if(!Male_radio_btn.isSelected() || !Female_radio_btn.isSelected() || !Non_binary_radio_btn.isSelected()){
+                genderLabel.setStyle("-fx-text-box-border: #ff0000; -fx-focus-color: #ff0000;");
+            }
+
+        } else {
+            verifySSNLabel.setText("");
+            verifyPhoneLabel.setText("");
+            verifyAgeLabel.setText("");
+            saveInfo(actionEvent);
+        }
+    }
+
+    //Methods that checks for age ; ssn & phone = number ; if user exists
+    public boolean checkSSNInfo() throws Exception, ArithmeticException {
+
+        try {
+            int intValue = Integer.parseInt(Ssn_fld.getText());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean checkPhoneInfo() throws Exception, ArithmeticException {
+
+        try {
+            int intValue = Integer.parseInt(Phone_no_fld.getText());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
+    public boolean ageVerify() {
+        LocalDate today = LocalDate.now();
+        LocalDate birthday = LocalDate.of(DOB_fld.getValue().getYear(),
+                DOB_fld.getValue().getMonth(),
+                DOB_fld.getValue().getDayOfMonth());
+        LocalDate verifyAge = today.minusYears(birthday.getYear());
+        if (verifyAge.getYear() < 16) {
+            return false;
+        } else if (verifyAge.getYear() == 16 && today.getMonthValue() < birthday.getMonthValue()) {
+            return false;
+        } else if (verifyAge.getYear() == 16 && today.getMonthValue() == birthday.getMonthValue() && today.getDayOfMonth() < birthday.getDayOfMonth()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public String genderCheck() {
+
+        if (Male_radio_btn.isSelected()) {
+            genderSet = "M";
+        } else if (Female_radio_btn.isSelected()) {
+            genderSet = "F";
+
+        } else if (Non_binary_radio_btn.isSelected()) {
+            genderSet = "O";
+        }
+        return genderSet;
+    }
+
+    public String maritalCheck() {
+
+        if (Single_radio_btn.isSelected()) {
+            maritalSet = "single";
+        } else if (Married_radio_btn.isSelected()) {
+            maritalSet = "married";
+        }
+        return maritalSet;
+    }
+
+
+    //Method that saves information from user input
+    public void saveInfo(ActionEvent actionEvent) throws SQLException, IOException {
+        Connectivity connectivity = new Connectivity();
+        Connection connection = connectivity.getConnection();
+        Customer customer = new Customer(FName_fld.getText(),
+                LName_fld.getText(),
+                MName_fld.getText(),
+                LocalDate.of(DOB_fld.getValue().getYear(),
+                        DOB_fld.getValue().getMonth(),
+                        DOB_fld.getValue().getDayOfMonth()),
+                Ssn_fld.getText(),
+                Phone_no_fld.getText(),
+                email_fld.getText(),
+                username_fld.getText(),
+                password_fld.getText(),
+                Address_fld.getText(),
+                city_fld.getText(),
+                state_ComboBox.getValue(),
+                zipcode_fld.getText(),
+                Apartment_fld.getText(),genderCheck(), maritalCheck()
+        );
+        System.out.println("You have successfully added " + customer);
+
+        String query = "insert into customer_personal_info(" + "First_name,Last_name,middle_name,date_of_birth,address,zipp_code,Country,states,city,contact_no, ssn,username,password,email,gender,marital_status) values "
+                + "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, customer.getFirstName());
+        statement.setString(2, customer.getLastName());
+        statement.setString(3, customer.getMiddleName());
+        statement.setString(4, customer.getDateOfBirth().toString());
+        statement.setString(5, customer.getStreet());
+        statement.setString(6, customer.getZipCode());
+        statement.setString(7, "US");
+        statement.setString(8, customer.getState());
+        statement.setString(9, customer.getCity());
+        statement.setString(10, customer.getPhoneNumber());
+        statement.setString(11, customer.getSSN());
+        statement.setString(12, customer.getUsername());
+        statement.setString(13, customer.getPassword());
+        statement.setString(14, customer.getEmail());
+        statement.setString(15, customer.getGender());
+        statement.setString(16, customer.getMartialStatus());
+        statement.executeUpdate();
+        System.out.println(customer.getEmail());
+        connection.close();
+    }
+
+    public void CheckIfUserExists() throws SQLException {
+        Connectivity connectivity = new Connectivity();
+        Connection connection = connectivity.getConnection();
+        String query = "SELECT * FROM customer_personal_info WHERE ssn = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, Ssn_fld.getText());
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+
+        }
+    }
 
 
     @Override
@@ -181,6 +489,8 @@ public class AdminDashController implements Initializable {
             throw new RuntimeException(e);
         }
 
+
+
         col_accnum.setCellValueFactory(new PropertyValueFactory<>("Account_number"));
         col_fname.setCellValueFactory(new PropertyValueFactory<>("First_name"));
         col_lname.setCellValueFactory(new PropertyValueFactory<>("Last_name"));
@@ -197,5 +507,8 @@ public class AdminDashController implements Initializable {
         col_email.setCellValueFactory(new PropertyValueFactory<>("email"));
 
         table.setItems(clientsList);
+        state_ComboBox.setItems(stateList);
+
     }
+
 }
