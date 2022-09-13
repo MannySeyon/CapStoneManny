422c872974c5a746477b4db1b2375aea3d8e0a5e
DHall0 <110750425+DHall0@users.noreply.github.com>
2022-09-13 14:24:25 -0500

    davidtablesupdate

--- src/main/java/com/example/demo/Admin/CheckingView.java
+++ src/main/java/com/example/demo/Admin/CheckingView.java
@@ -7,7 +7,7 @@ public class CheckingView {
 
     public CheckingView(String checking_acc_number, String account_number, String account_bal) {
 
-        this. Checking_account_number =new SimpleStringProperty(checking_acc_number);
+        this.Checking_account_number =new SimpleStringProperty(checking_acc_number);
         this.Account_number =new SimpleStringProperty(account_number);
         this.account_balance =new SimpleStringProperty(account_bal);
 
