8030a0283930f300c8038946f715f7aeb7bad04e
Pandora Vang <pandora.vaj@gmail.com>
2022-09-14 06:24:35 -0500

    Service 8.0 AboutUs.fxml - Textfield reentry - Home button AboutUsController - Home button

--- src/main/java/com/example/demo/AboutUsController.java
+++ src/main/java/com/example/demo/AboutUsController.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AboutUsController {
    @FXML
@@ -15,4 +23,15 @@ public class AboutUsController {
     private  Pane chouaText,pandoraText,mannyText,samText,ruhamaText,DavidText;
     @FXML
     private  ImageView chouaImage,panImage, mannyImage,ruhImage,Image2,Image;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    protected void Home(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("MainModified.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
