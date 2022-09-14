86963dc256f8d7c43e116c39ff14061b8b96a89d
Pandora Vang <pandora.vaj@gmail.com>
2022-09-14 10:27:23 -0500

    Service 8.2 -chatbot button to dashboard

--- src/main/java/com/example/demo/ChatbotController.java
+++ src/main/java/com/example/demo/ChatbotController.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -11,6 +15,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class ChatbotController {
     @FXML
@@ -33,22 +41,37 @@ public class ChatbotController {
     private Button sendButton;
     @FXML
     private  FontAwesomeIconView minIcon;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    protected void Home(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LandingPage.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     public void SendMessage(ActionEvent event) {
+
+
         if (userInput.getText().contains("hi") || userInput.getText().contains("hello") || userInput.getText().contains("hey") || userInput.getText().contains("howdy")) {
             Console.setText("Hello, this is Summit Financial Corp. ChatBot. How may I assist you?");
         }
-        if (userInput.getText().contains("transfer funds")){
+        else if (userInput.getText().contains("transfer funds")){
             Console.setText("You can transfer funds by clicking on the Transaction button on your dashboard page.");
         }
-        if(userInput.getText().contains("mobile deposit")){
+        else if(userInput.getText().contains("mobile deposit")){
             Console.setText("Summit Financial Corp. does not support mobile deposit.");
         }
-        if (userInput.getText().contains("closing account")){
+        else if (userInput.getText().contains("closing account")){
             Console.setText("You will have to go to the nearest Summit Financial Corp. branch and speak with a bank teller for more information.");
         }
         else{
-        Console.setText("");
+
+                Console.setText("Oops. I didn't get that!");
         }
+
     }
 }
 
